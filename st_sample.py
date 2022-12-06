1a2793477a9bfe3e11589059b53c1f09d4816e8d
ayitesarp <119827885+ayitesarp@users.noreply.github.com>
2022-12-06 19:24:00 +0700

    Update st_sample.py

--- st_sample.py
+++ st_sample.py
@@ -25,7 +25,7 @@ def main():
     st.sidebar.title("Pengaturan Constraints")
     p = st.sidebar.number_input(label="Fungsi Pendidikan (dalam %)",value=20,min_value=20, max_value=100, step=1)
     k= st.sidebar.number_input(label="Fungsi Kesehatan (dalam %)",value=10,min_value=10, max_value=100, step=1)
-    l = st.sidebar.number_input(label="Fungsi Pelayanan Umum (dalam %)",value=30,min_value=1, max_value=100, step=1)
+    l = st.sidebar.number_input(label="Fungsi Perumahan dan Fasilitas Umum (dalam %)",value=25,min_value=1, max_value=100, step=1)
         
     df = pd.read_excel('data_sumber.xlsx')
     pemda = st.selectbox('Pilih Pemerintah Daerah', df['Pemda'].unique())
@@ -59,6 +59,9 @@ def main():
 
     with col2:
         #Nilai ambil dari nilai realisasi pemda sesuai filter
+        didik = pilihan.iloc[0,9]
+        sehat = pilihan.iloc[0,7]
+        pelayanan = pilihan.iloc[0,1]
         tertib = pilihan.iloc[0,3]
         eko = pilihan.iloc[0,4]
         lh = pilihan.iloc[0,5]
@@ -85,8 +88,14 @@ def main():
         # Add the constraints to the model
         prob += (x1 >= p*totalbelanja/100, "pendidikan_constraint")
         prob += (x2 >= k*totalbelanja/100, "kesehatan_constraint")
-        prob += (x3 >= l*totalbelanja/100, "pelayanan_constraint")
+        prob += (x8 >= l*totalbelanja/100, "perumahan_constraint")
         # Tidak boleh turun lebih dari 5% dan naik lebih dari 10% untuk 6 fungsi lainnya
+        prob += (x1 >= 0.95*didik,"pendidikan_bawah_constraint")
+        prob += (x1 <= 1.1*didik,"pendidikan_atas_constraint")
+        prob += (x2 >= 0.95*sehat,"kesehatan_bawah_constraint")
+        prob += (x2 <= 1.1*sehat,"kesehatan_atas_constraint")
+        prob += (x3 >= 0.95*pelayanan,"pelayanan_bawah_constraint")
+        prob += (x3 <= 1.1*pelayanan,"pelayanan_atas_constraint")        
         prob += (x4 >= 0.95*wisata,"wisata_bawah_constraint")
         prob += (x4 <= 1.1*wisata,"wisata_atas_constraint")
         prob += (x5 >= 0.95*tertib,"tertib_bawah_constraint")
@@ -95,8 +104,8 @@ def main():
         prob += (x6 <= 1.1*linsos,"linsos_atas_constraint")
         prob += (x7 >= 0.95*lh,"lh_bawah_constraint")
         prob += (x7 <= 1.1*lh,"lh_atas_constraint")
-        prob += (x8 >= 0.95*perum,"perum_bawah_constraint")
-        prob += (x8 <= 1.1*perum,"perum_atas_constraint")
+        prob += (x3 >= 0.95*,"pelayanan_bawah_constraint")
+        prob += (x3 <= 1.1*perum,"perum_atas_constraint")
         prob += (x9 >= 0.95*eko,"eko_bawah_constraint")
         prob += (x9 <= 1.1*eko,"eko_atas_constraint")
         #Total belanja 9 fungsi tidak boleh lebih dari total belanja
